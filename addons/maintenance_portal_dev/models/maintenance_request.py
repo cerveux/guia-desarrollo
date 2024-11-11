3930db1d42b383b173518c73cc239e09af066bfc
dguzman-betech <dguzman.betech@gmail.com>
2024-11-11 13:53:51 -0300

    feat(mantenimiento): add department_id

--- addons/maintenance_portal_dev/models/maintenance_request.py
+++ addons/maintenance_portal_dev/models/maintenance_request.py
@@ -9,3 +9,8 @@ class MaintenanceRequest(models.Model):
     _inherit = "maintenance.request"
 
     partner_id = fields.Many2one('res.partner', string='Usuario Portal Solicitante' )
+    department_id = fields.Many2one('hr.department', readonly=True, string='Departamento')
+    
+    @api.onchange('equipment_id')
+    def onchange_equipment_id(self):
+        self.department_id = self.equipment_id.department_id
