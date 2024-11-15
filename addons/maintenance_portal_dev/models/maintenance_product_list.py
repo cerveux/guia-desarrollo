00d871d9d51635e2c800c2416fd04be70dfd811a
dguzman-betech <dguzman.betech@gmail.com>
2024-11-15 14:51:09 -0300

    fix() branches

--- addons/maintenance_portal_dev/models/maintenance_product_list.py
+++ addons/maintenance_portal_dev/models/maintenance_product_list.py
@@ -10,5 +10,6 @@ class maintenance_product_list(models.Model):
     product_id = fields.Many2one('product.product', string='Producto', required=True)
     quantity = fields.Integer(required=True, string= 'Cantidad')
     request_id = fields.Many2one( "maintenance.request", string="Pedido Mantenimiento", required=True )
+    equipment_id = fields.Many2one( "maintenance.equipment", string="Equipos de Mantenimiento", required=True )
     uom_id = fields.Many2one(related='product_id.uom_id', string='Unidad de medida')
     
