e2740ad1cd45fa2be504e72428479e350de79729
dguzman-betech <dguzman.betech@gmail.com>
2024-11-21 11:40:01 -0300

    feat(portal): create maintenance request button on portal

--- addons/maintenance_portal_dev/controllers/controllers.py
+++ addons/maintenance_portal_dev/controllers/controllers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import http
-from odoo.http import request
+from odoo.http import request, route
 from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
@@ -11,4 +11,3 @@ class MaintenancePortalDev(CustomerPortal):
         maintenance_count = request.env["maintenance.request"].search_count([])
         values["maintenance_count"] = maintenance_count
         return values
-        
