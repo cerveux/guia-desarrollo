0573ee127891f874e6131d85dd37d3865ba597d3
dguzman-betech <dguzman.betech@gmail.com>
2024-11-26 10:33:12 -0300

    feat(helpdesk): populate product list from maintenance request

--- addons/helpdesk_mgmt_mod/models/helpdesk_ticket.py
+++ addons/helpdesk_mgmt_mod/models/helpdesk_ticket.py
@@ -10,3 +10,8 @@ class HelpdeskTicket(models.Model):
     
     product_structure_ids = fields.One2many('maintenance.product.list', 'helpdesk_ticket_id', string='Listado de productos')
     maintenance_request_id = fields.Many2one('maintenance.request', string='Petición de mantenimiento')
+
+    @api.onchange('maintenance_request_id')
+    def _onchange_maintenance_request_id(self):
+        for rec in self:
+            rec.product_structure_ids = rec.maintenance_request_id.maintenance_product_list
