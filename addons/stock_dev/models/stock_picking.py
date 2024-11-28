bb1d4e8510ac72eade194048f6655478b25da8f0
dguzman-betech <dguzman.betech@gmail.com>
2024-11-28 00:24:15 -0300

    feat(stock): populate stock move products with ticket products list

--- addons/stock_dev/models/stock_picking.py
+++ addons/stock_dev/models/stock_picking.py
@@ -8,4 +8,23 @@ class Picking(models.Model):
 
     _inherit = "stock.picking"
     
-    help_desk_ticket_id = fields.Many2one('helpdesk.ticket', string='Ticket relacionado')
+    helpdesk_ticket_id = fields.Many2one('helpdesk.ticket', string='Ticket relacionado')
+    
+    
+    @api.onchange('helpdesk_ticket_id')
+    def _onchange_maintenance_request_id(self):
+        if self.helpdesk_ticket_id:
+            # Clear existing moves
+            self.move_ids_without_package = [(5, 0, 0)]
+
+            product_moves = []
+            for product in self.helpdesk_ticket_id.product_structure_ids:
+                product_moves.append((0, 0, {
+                    'product_id': product.product_id.id,
+                    'product_uom_qty': product.quantity,
+                    'name': product.product_id.name,
+                    'product_uom': product.uom_id.id,
+                    'location_id': self.location_id,
+                    'location_dest_id': self.location_dest_id
+                }))
+            self.move_ids_without_package = product_moves
